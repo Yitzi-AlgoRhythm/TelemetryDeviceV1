e51fc48267252d473b64bb51bab59f0011ca551c
Yitzi Synett <synettyitzchak@gmail.com>
2026-08-31 11:47:28 +0300

    Add Builder Stage

--- Dataflow/Stages/IDecoderStage.cs
+++ Dataflow/Stages/IDecoderStage.cs
@@ -4,6 +4,6 @@ namespace TelemetryDeviceV1.Dataflow.Stages
 {
     public interface IDecoderStage
     {
-        public ParameterData Decode(IEnumerable<byte> data);
+        public IEnumerable<ParameterData> Decode(IEnumerable<byte> data);
     }
 }
---
+++ Deserialization/IcdConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace TelemetryDeviceV1.Deserialization
+{
+    public static class IcdConstants
+    {
+        public static readonly byte[] SyncBytes = { 66, 32, 23 };
+    }
+}
